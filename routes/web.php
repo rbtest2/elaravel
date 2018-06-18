b7c1cba3874a3923af93f393bb58d8db7c0b3d18
Ron <cwheavyload23@gmail.con>
2018-06-18 01:35:58 -0500

    v.1

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//frontend sites
+Route::get('/','HomeController@index');
+
+
+
+
+//backend routes...
+Route::get('/logout','SuperAdminController@logout');
+Route::get('/admin','AdminController@index');
+Route::get('/dashboard','AdminController@show_dashboard');
+Route::post('/admin-dashboard','AdminController@dashboard');
+
+//Category related route
+Route::get('/add-category','CategoryController@index');
+Route::get('/all-category','CategoryController@all_category');
